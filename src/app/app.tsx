48577b35ad10399aa81b140898e619ad355f18d0
sahara.ap <sahara.ap13@gmail.com>
2024-01-24 18:30:56 +0300

    Реализован компонент HistoryRouter

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,22 +1,24 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import { CatalogPage } from '../pages/catalog-page/catalog-page';
 import { SelectedProductPage } from '../pages/selected-product-page/selected-product-page';
 import { HelmetProvider } from 'react-helmet-async';
 
 import { AppRoute } from '../consts';
 import { NotFoundPage } from '../pages/not-found-page/not-found-page';
+import HistoryRouter from '../components/history-router';
+import browserHistory from '../browser-history';
 
 
 function App(): JSX.Element {
   return (
     <HelmetProvider>
-      <BrowserRouter>
+      <HistoryRouter history={browserHistory}>
         <Routes>
           <Route path={AppRoute.Catalog} element={<CatalogPage />} />
           <Route path={AppRoute.Product} element={<SelectedProductPage />} />
           <Route path={AppRoute.NotFound} element={<NotFoundPage />} />
         </Routes>
-      </BrowserRouter>
+      </HistoryRouter>
     </HelmetProvider>
   );
 }
