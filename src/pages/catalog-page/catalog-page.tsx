9a3baf1e5c5dea84bc6d2a3bedcc5907967588a2
sahara.ap <sahara.ap13@gmail.com>
2024-01-25 10:45:23 +0300

    Декомпозирован компонент CatalogFilterAside

--- src/pages/catalog-page/catalog-page.tsx
+++ src/pages/catalog-page/catalog-page.tsx
@@ -1,5 +1,6 @@
 import { Banner } from '../../components/banner/banner';
 import { Breadcrumbs } from '../../components/breadcrumbs/breadcrumbs';
+import { CatalogFilterAside } from '../../components/catalog-filter-aside/catalog-filter-aside';
 import { Footer } from '../../components/footer/footer';
 import { Header } from '../../components/header/header';
 import { AppRoute } from '../../consts';
@@ -16,84 +17,7 @@ function CatalogPage(): JSX.Element {
             <div className="container">
               <h1 className="title title--h2">Каталог фото- и видеотехники</h1>
               <div className="page-content__columns">
-                <div className="catalog__aside">
-                  <div className="catalog-filter">
-                    <form action="#">
-                      <h2 className="visually-hidden">Фильтр</h2>
-                      <fieldset className="catalog-filter__block">
-                        <legend className="title title--h5">Цена, ₽</legend>
-                        <div className="catalog-filter__price-range">
-                          <div className="custom-input">
-                            <label>
-                              <input type="number" name="price" placeholder="от" />
-                            </label>
-                          </div>
-                          <div className="custom-input">
-                            <label>
-                              <input type="number" name="priceUp" placeholder="до" />
-                            </label>
-                          </div>
-                        </div>
-                      </fieldset>
-                      <fieldset className="catalog-filter__block">
-                        <legend className="title title--h5">Категория</legend>
-                        <div className="custom-checkbox catalog-filter__item">
-                          <label>
-                            <input type="checkbox" name="photocamera" defaultChecked /><span className="custom-checkbox__icon"></span><span className="custom-checkbox__label">Фотокамера</span>
-                          </label>
-                        </div>
-                        <div className="custom-checkbox catalog-filter__item">
-                          <label>
-                            <input type="checkbox" name="videocamera" /><span className="custom-checkbox__icon"></span><span className="custom-checkbox__label">Видеокамера</span>
-                          </label>
-                        </div>
-                      </fieldset>
-                      <fieldset className="catalog-filter__block">
-                        <legend className="title title--h5">Тип камеры</legend>
-                        <div className="custom-checkbox catalog-filter__item">
-                          <label>
-                            <input type="checkbox" name="digital" defaultChecked /><span className="custom-checkbox__icon"></span><span className="custom-checkbox__label">Цифровая</span>
-                          </label>
-                        </div>
-                        <div className="custom-checkbox catalog-filter__item">
-                          <label>
-                            <input type="checkbox" name="film" disabled /><span className="custom-checkbox__icon"></span><span className="custom-checkbox__label">Плёночная</span>
-                          </label>
-                        </div>
-                        <div className="custom-checkbox catalog-filter__item">
-                          <label>
-                            <input type="checkbox" name="snapshot" /><span className="custom-checkbox__icon"></span><span className="custom-checkbox__label">Моментальная</span>
-                          </label>
-                        </div>
-                        <div className="custom-checkbox catalog-filter__item">
-                          <label>
-                            <input type="checkbox" name="collection" defaultChecked disabled /><span className="custom-checkbox__icon"></span><span className="custom-checkbox__label">Коллекционная</span>
-                          </label>
-                        </div>
-                      </fieldset>
-                      <fieldset className="catalog-filter__block">
-                        <legend className="title title--h5">Уровень</legend>
-                        <div className="custom-checkbox catalog-filter__item">
-                          <label>
-                            <input type="checkbox" name="zero" defaultChecked /><span className="custom-checkbox__icon"></span><span className="custom-checkbox__label">Нулевой</span>
-                          </label>
-                        </div>
-                        <div className="custom-checkbox catalog-filter__item">
-                          <label>
-                            <input type="checkbox" name="non-professional" /><span className="custom-checkbox__icon"></span><span className="custom-checkbox__label">Любительский</span>
-                          </label>
-                        </div>
-                        <div className="custom-checkbox catalog-filter__item">
-                          <label>
-                            <input type="checkbox" name="professional" /><span className="custom-checkbox__icon"></span><span className="custom-checkbox__label">Профессиональный</span>
-                          </label>
-                        </div>
-                      </fieldset>
-                      <button className="btn catalog-filter__reset-btn" type="reset">Сбросить фильтры
-                      </button>
-                    </form>
-                  </div>
-                </div>
+                <CatalogFilterAside />
                 <div className="catalog__content">
                   <div className="catalog-sort">
                     <form action="#">
