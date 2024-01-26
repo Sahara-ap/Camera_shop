5c561f463485db95e70428e65cf836c67bcc50b7
sahara.ap <sahara.ap13@gmail.com>
2024-01-26 23:29:01 +0300

    Реализована динамическая отрисовка контента

--- src/components/selected-product-info/selected-product-info.tsx
+++ src/components/selected-product-info/selected-product-info.tsx
@@ -1,16 +1,24 @@
+import { useAppSelector } from '../../hooks/store-hooks';
+import { getSelectedCamera } from '../../store/selected-card-data-store/selected-card-data-selectors';
 
-function SelectedProductInfo(): JSX.Element {
+function SelectedProductInfo(): JSX.Element | null {
+  const info = useAppSelector(getSelectedCamera);
+
+  if (!info) {
+    return null;
+  }
   return (
     <div className="page-content__section">
       <section className="product">
         <div className="container">
           <div className="product__img">
             <picture>
-              <source type="image/webp" srcSet="img/content/das-auge.webp, img/content/das-auge@2x.webp 2x" /><img src="img/content/das-auge.jpg" srcSet="img/content/das-auge@2x.jpg 2x" width="560" height="480" alt="Ретрокамера Das Auge IV" />
+              <source type="image/webp" srcSet={`${info.previewImgWebp}, ${info.previewImgWebp2x} 2x`} />
+              <img src={info.previewImg} srcSet={`${info.previewImgWebp2x} 2x`} width="560" height="480" alt={info.name} />
             </picture>
           </div>
           <div className="product__content">
-            <h1 className="title title--h3">Ретрокамера Das Auge IV</h1>
+            <h1 className="title title--h3">{info.name}</h1>
             <div className="rate product__rate">
               <svg width="17" height="16" aria-hidden="true">
                 <use xlinkHref="#icon-full-star"></use>
@@ -27,10 +35,10 @@ function SelectedProductInfo(): JSX.Element {
               <svg width="17" height="16" aria-hidden="true">
                 <use xlinkHref="#icon-star"></use>
               </svg>
-              <p className="visually-hidden">Рейтинг: 4</p>
-              <p className="rate__count"><span className="visually-hidden">Всего оценок:</span>12</p>
+              <p className="visually-hidden">Рейтинг: {info.rating}</p>
+              <p className="rate__count"><span className="visually-hidden">Всего оценок:</span>{info.reviewCount}</p>
             </div>
-            <p className="product__price"><span className="visually-hidden">Цена:</span>73 450 ₽</p>
+            <p className="product__price"><span className="visually-hidden">Цена:</span>{info.price} ₽</p>
             <button className="btn btn--purple" type="button">
               <svg width="24" height="16" aria-hidden="true">
                 <use xlinkHref="#icon-add-basket"></use>
@@ -45,23 +53,22 @@ function SelectedProductInfo(): JSX.Element {
                 <div className="tabs__element">
                   <ul className="product__tabs-list">
                     <li className="item-list"><span className="item-list__title">Артикул:</span>
-                      <p className="item-list__text"> DA4IU67AD5</p>
+                      <p className="item-list__text"> {info.vendorCode}</p>
                     </li>
                     <li className="item-list"><span className="item-list__title">Категория:</span>
-                      <p className="item-list__text">Видеокамера</p>
+                      <p className="item-list__text">{info.category}</p>
                     </li>
                     <li className="item-list"><span className="item-list__title">Тип камеры:</span>
-                      <p className="item-list__text">Коллекционная</p>
+                      <p className="item-list__text">{info.type}</p>
                     </li>
                     <li className="item-list"><span className="item-list__title">Уровень:</span>
-                      <p className="item-list__text">Любительский</p>
+                      <p className="item-list__text">{info.level}</p>
                     </li>
                   </ul>
                 </div>
                 <div className="tabs__element is-active">
                   <div className="product__tabs-text">
-                    <p>Немецкий концерн BRW разработал видеокамеру Das Auge IV в&nbsp;начале 80-х годов, однако она до&nbsp;сих пор пользуется популярностью среди коллекционеров и&nbsp;яростных почитателей старинной техники.</p>
-                    <p>Вы&nbsp;тоже можете прикоснуться к&nbsp;волшебству аналоговой съёмки, заказав этот чудо-аппарат. Кто знает, может с&nbsp;Das Auge IV&nbsp;начнётся ваш путь к&nbsp;наградам всех престижных кинофестивалей.</p>
+                    <p>{info.description}</p>
                   </div>
                 </div>
               </div>
