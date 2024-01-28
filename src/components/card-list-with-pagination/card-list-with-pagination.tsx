8db83db6b46c92137cb054656c3c635873509018
sahara.ap <sahara.ap13@gmail.com>
2024-01-28 20:22:17 +0300

    Реализована пагинация главной страницы согласно ТЗ

--- src/components/card-list-with-pagination/card-list-with-pagination.tsx
+++ src/components/card-list-with-pagination/card-list-with-pagination.tsx
@@ -1,16 +1,21 @@
-import { CardList } from '../card-list/card-list';
-import { useAppSelector } from '../../hooks/store-hooks';
+import { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
 import { getCameras, getIsCamerasLoading } from '../../store/cards-data-store/cards-data-selectors';
-import { useState } from 'react';
+import { useAppSelector } from '../../hooks/store-hooks';
+
+import { CardList } from '../card-list/card-list';
 import { Loading } from '../loading/loading';
 import { CatalogPagination } from '../catalog-pagination/catalog-pagination';
 
+import { AppRoute } from '../../consts';
+
 const DEFAULT_PAGE_NUMBER = 1;
 const CARDS_NUMBER_PER_PAGE = 9;
+const MIN_PAGES = 2;
 
 function CardListWithPagination(): JSX.Element {
   const cameras = useAppSelector(getCameras);
-
   const totalCardsLength = cameras.length;
   const totalPages = Math.ceil(totalCardsLength / CARDS_NUMBER_PER_PAGE);
 
@@ -19,8 +24,19 @@ function CardListWithPagination(): JSX.Element {
   const end = CARDS_NUMBER_PER_PAGE * pageNumber;
   const currentCameras = cameras.slice(start, end);
 
-
   const isCameraLoading = useAppSelector(getIsCamerasLoading);
+
+  const navigate = useNavigate();
+  useEffect(() => {
+    let isMounted = true;
+    if (isMounted) {
+      navigate(`${AppRoute.Catalog}?page=${DEFAULT_PAGE_NUMBER}`);
+    }
+    return () => {
+      isMounted = false;
+    };
+
+  }, [navigate]);
 
   if (isCameraLoading) {
     return <Loading />;
@@ -28,12 +44,12 @@ function CardListWithPagination(): JSX.Element {
   return (
     <>
       <CardList cards={currentCameras} />
-      <CatalogPagination
-        totalCardsLength={totalCardsLength}
-        totalPages={totalPages}
-        pageNumber={pageNumber}
-        onPaginationClick={setPageNumber}
-      />
+      {totalPages >= MIN_PAGES &&
+        <CatalogPagination
+          totalPages={totalPages}
+          pageNumber={pageNumber}
+          onPaginationClick={setPageNumber}
+        />}
     </>
   );
 }
--- src/components/catalog-pagination/catalog-pagination.tsx
+++ src/components/catalog-pagination/catalog-pagination.tsx
@@ -1,40 +1,27 @@
-import { Link, useNavigate } from 'react-router-dom';
-import { AppRoute } from '../../consts';
-import { useEffect } from 'react';
+import { Link } from 'react-router-dom';
 import cn from 'classnames';
 
+import { AppRoute } from '../../consts';
+
+const STEP = 1;
+
 type TCatalogPaginationProps = {
-  totalCardsLength: number;
   totalPages: number;
   pageNumber: number;
   onPaginationClick: (pageNumber: number) => void;
 
 }
-function CatalogPagination({ totalCardsLength, totalPages, pageNumber, onPaginationClick }: TCatalogPaginationProps): JSX.Element | null {
+function CatalogPagination({ totalPages, pageNumber, onPaginationClick }: TCatalogPaginationProps): JSX.Element | null {
+
+  const lastPage = totalPages;
 
   function handlePaginationClick(page: number) {
     onPaginationClick(page);
   }
 
-  function getUrl() {
-    return new URL(window.location.href);
-  }
-
-  function getParams() {
-    const url = getUrl();
-    const params = Object.fromEntries(url.searchParams);
-
-    return params;
-  }
-
-  function setParams(page: number) {
-    const url = getUrl();
-    url.search = '';
-    url.searchParams.set('page', String(page));
-  }
-
   function getPaginationLength(pages: number) {
     switch (pages) {
+      case 0:
       case 1:
         return;
       case 2:
@@ -46,40 +33,42 @@ function CatalogPagination({ totalCardsLength, totalPages, pageNumber, onPaginat
 
   const length = getPaginationLength(totalPages);
   const paginationRange = new Array(length).fill(null).map((_, index) => {
-    const value = Number(index + pageNumber);
-    return value;
+    let result;
+    switch (pageNumber) {
+      case 1:
+        result = index + pageNumber;
+        break;
+      case lastPage:
+        result = (index - 2) + pageNumber;
+        break;
+      default:
+        result = (index - 1) + pageNumber;
+    }
+
+    return result;
   });
 
-  const navigate = useNavigate();
-  useEffect(() => {
-    navigate(`${AppRoute.Catalog}?page=${pageNumber}`);
 
-  }, [pageNumber, navigate]);
-
-  const { page } = getParams();
-  console.log('page', page);
-
-
-
-  const currentPageNumbers = 1
-
-
-  if (totalCardsLength <= 9) {
-    return null;
-  }
   return (
     <div className="pagination">
       <ul className="pagination__list">
+        {totalPages >= 4 && pageNumber >= 3 &&
+          <li className="pagination__item">
+            <Link
+              className="pagination__link pagination__link--text"
+              to={`${AppRoute.Catalog}?page${paginationRange[0] - STEP}`}
+              onClick={() => handlePaginationClick(paginationRange[0] - STEP)}
+            >Назад
+            </Link>
+          </li>}
+
         {paginationRange.map((value) => (
           <li
-            key={crypto.randomUUID()}
+            key={value}
             className="pagination__item"
           >
             <Link
-              className={cn(
-                'pagination__link',
-                { 'pagination__link--active': value === pageNumber }
-              )}
+              className={cn('pagination__link', { 'pagination__link--active': value === pageNumber })}
               to={`${AppRoute.Catalog}?page=${value}`}
               onClick={() => handlePaginationClick(value)}
             >
@@ -87,20 +76,16 @@ function CatalogPagination({ totalCardsLength, totalPages, pageNumber, onPaginat
             </Link>
           </li>
         ))}
-        {totalPages >= 4 &&
-          <li className="pagination__item">
-            <Link className="pagination__link pagination__link--text" to="2">Далее</Link>
-          </li>
-        }
 
-        {/* <li className="pagination__item"><Link className="pagination__link pagination__link--active" to="1">1</Link>
-        </li>
-        <li className="pagination__item"><Link className="pagination__link" to="2">2</Link>
-        </li>
-        <li className="pagination__item"><Link className="pagination__link" to="3">3</Link>
-        </li>
-        <li className="pagination__item"><Link className="pagination__link pagination__link--text" to="2">Далее</Link>
-        </li> */}
+        {totalPages >= 4 && pageNumber < (lastPage - 1) &&
+          <li className="pagination__item">
+            <Link
+              className="pagination__link pagination__link--text"
+              to={`${AppRoute.Catalog}?page${paginationRange[paginationRange.length - 1] + STEP}`}
+              onClick={() => handlePaginationClick(paginationRange[paginationRange.length - 1] + STEP)}
+            >Далее
+            </Link>
+          </li>}
       </ul>
     </div>
   );
