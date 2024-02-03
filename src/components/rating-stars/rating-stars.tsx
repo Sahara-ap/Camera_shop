b8c58949ce02eced6cf624be5438fafe77a02a30
sahara.ap <sahara.ap13@gmail.com>
2024-02-03 17:46:50 +0300

    Реализован компонент RatingStars

---
+++ src/components/rating-stars/rating-stars.tsx
@@ -0,0 +1,24 @@
+type TRatingProps = {
+  rating: number;
+}
+
+
+function RatingStars({ rating }: TRatingProps): JSX.Element {
+  const ratingMap = [1, 2, 3, 4, 5];
+  const score = rating - 1;
+
+  return (
+    <>
+      {
+        ratingMap.map((_, index) => (
+          <svg key={crypto.randomUUID()} width="17" height="16" aria-hidden="true">
+            <use xlinkHref={index <= score ? '#icon-full-star' : '#icon-star'}></use>
+          </svg>
+        ))
+      }
+    </>
+  );
+}
+
+
+export { RatingStars };
