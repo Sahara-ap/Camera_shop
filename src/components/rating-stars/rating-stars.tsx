696f6f9905e6ff460465b6918df0aac2b17c9723
sahara.ap <sahara.ap13@gmail.com>
2024-02-15 15:32:27 +0300

    A7. Исправлена нотация константы

--- src/components/rating-stars/rating-stars.tsx
+++ src/components/rating-stars/rating-stars.tsx
@@ -2,17 +2,17 @@ type TRatingProps = {
   rating: number;
 }
 
+const MAX_NUMBER_OF_STARS = 5;
 
 function RatingStars({ rating }: TRatingProps): JSX.Element {
-  const ratingMap = [1, 2, 3, 4, 5];
-  const score = rating - 1;
+  const relevantIndexForRatingValue = rating - 1;
 
   return (
     <>
       {
-        ratingMap.map((_, index) => (
+        new Array(MAX_NUMBER_OF_STARS).fill(null).map((_, index) => (
           <svg key={crypto.randomUUID()} width="17" height="16" aria-hidden="true" data-testid="ratingStarsSvg">
-            <use xlinkHref={index <= score ? '#icon-full-star' : '#icon-star'}></use>
+            <use xlinkHref={index <= relevantIndexForRatingValue ? '#icon-full-star' : '#icon-star'}></use>
           </svg>
         ))
       }
