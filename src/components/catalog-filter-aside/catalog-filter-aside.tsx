1e52d65e65689b47a15a3678824d7e6570932326
Sahara <sahara.ap13@gmail.com>
2024-02-23 22:24:54 +0300

    Реализован сброс фильтров по кнопке reset

--- src/components/catalog-filter-aside/catalog-filter-aside.tsx
+++ src/components/catalog-filter-aside/catalog-filter-aside.tsx
@@ -2,14 +2,31 @@ import { FilterCategory } from './filter-category/filter-category';
 import { FilterType } from './filter-type/filter-type';
 import { FilterLevel } from './filter-level/filter-level';
 import { FilterPrice } from './filter-price/filter-price';
+import { useRef } from 'react';
+import { useSearchParams } from 'react-router-dom';
+import { getParams } from '../../utils/utils-functions';
 
 function CatalogFilterAside(): JSX.Element {
+  const formRef = useRef<HTMLFormElement | null>(null);
+  const [searchParams, setSearchParams] = useSearchParams();
+  const params = getParams(searchParams);
 
+  function handleResetClick() {
+    delete params.cat;
+    delete params.type;
+    delete params.level;
+    delete params.priceMin;
+    delete params.priceMax;
 
+    setSearchParams(params);
+  }
   return (
     <div className="catalog__aside">
       <div className="catalog-filter">
-        <form action="#">
+        <form
+          action="#"
+          ref={formRef}
+        >
           <h2 className="visually-hidden">Фильтр</h2>
           <FilterPrice />
 
@@ -18,7 +35,12 @@ function CatalogFilterAside(): JSX.Element {
           <FilterLevel />
 
 
-          <button className="btn catalog-filter__reset-btn" type="reset">Сбросить фильтры
+          <button
+            className="btn catalog-filter__reset-btn"
+            type="reset"
+            onClick={handleResetClick}
+          >
+            Сбросить фильтры
           </button>
         </form>
       </div>
