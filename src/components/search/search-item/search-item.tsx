bdab69476f9b46437e46d9dcb67669562b76dbf0
Sahara <sahara.ap13@gmail.com>
2024-02-25 20:36:39 +0300

    Реализован переход на карточку товара по клику

--- src/components/search/search-item/search-item.tsx
+++ src/components/search/search-item/search-item.tsx
@@ -1,11 +1,18 @@
+import { Link } from 'react-router-dom';
 import { TCard } from '../../../types/general-types';
+import { AppRoute } from '../../../consts';
 
 type TSearchItemProps = {
   item: TCard;
 }
-function SearchItem({item}: TSearchItemProps): JSX.Element {
+function SearchItem({ item }: TSearchItemProps): JSX.Element {
   return (
-    <li className="form-search__select-item" tabIndex={0}>{item.name}</li>
+    <Link
+      to={`${AppRoute.Product}/${item.id}`}
+    >
+      <li className="form-search__select-item" tabIndex={0}>{item.name}</li>
+    </Link>
+
   );
 }
 
