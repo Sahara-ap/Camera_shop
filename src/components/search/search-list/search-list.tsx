2e94431af286527d9ff76d7577ba6a55382c042d
Sahara <sahara.ap13@gmail.com>
2024-02-25 20:36:21 +0300

    fix. Удален закомментированный код

--- src/components/search/search-list/search-list.tsx
+++ src/components/search/search-list/search-list.tsx
@@ -16,8 +16,3 @@ function SearchList({ list }: TSearchListProps): JSX.Element {
 }
 
 export { SearchList };
-
-// <li className="form-search__select-item" tabIndex={0}>Cannonball Pro MX 7i</li>
-// <li className="form-search__select-item" tabIndex={0}>Cannonball Pro MX 6i</li>
-// <li className="form-search__select-item" tabIndex={0}>Cannonball Pro MX 5i</li>
-// <li className="form-search__select-item" tabIndex={0}>Cannonball Pro MX 4i</li>
