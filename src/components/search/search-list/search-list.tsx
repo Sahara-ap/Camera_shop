9cafc861cdc8eae7337100d3f7b61e397cf3daff
Sahara <sahara.ap13@gmail.com>
2024-02-25 22:31:01 +0300

    Реализовано закрытие SearchList при outsideClick

--- src/components/search/search-list/search-list.tsx
+++ src/components/search/search-list/search-list.tsx
@@ -1,13 +1,43 @@
+import { useEffect, useRef } from 'react';
 import { TCard } from '../../../types/general-types';
 import { SearchItem } from '../search-item/search-item';
 
 type TSearchListProps = {
   list: TCard[];
+  onSearchListClick: (value: string) => void;
 }
-function SearchList({ list }: TSearchListProps): JSX.Element {
+function SearchList({ list, onSearchListClick }: TSearchListProps): JSX.Element {
+  const searchListRef = useRef<HTMLUListElement>(null);
+  const closeSearchList = onSearchListClick;
+
+
+  function handleOutsideClick(event: MouseEvent) {
+    event.preventDefault();
+    const element = searchListRef.current;
+    if (element && !element.contains(event.target as Element)) {
+      closeSearchList('');
+    }
+
+  }
+  useEffect(() => {
+    let isMounted = true;
+    const element = searchListRef.current;
+
+    if (isMounted && element) {
+      document.addEventListener('click', handleOutsideClick);
+    }
+
+    return () => {
+      isMounted = false;
+      document.removeEventListener('click', handleOutsideClick);
+    };
+  });
 
   return (
-    <ul className="form-search__select-list">
+    <ul
+      className="form-search__select-list"
+      ref={searchListRef}
+    >
       {list.map((item) => (
         <SearchItem key={item.id} item={item} />
       ))}
