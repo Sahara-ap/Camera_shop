1757e0e0ef428c82140fd200d6f4d7439dbe668b
Sahara <sahara.ap13@gmail.com>
2024-02-26 14:44:59 +0300

    Перемещение логики закрытия click Outside на уровень выше

--- src/components/search/search-main/search-main.tsx
+++ src/components/search/search-main/search-main.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import cn from 'classnames';
 
 import { useAppSelector } from '../../../hooks/store-hooks';
@@ -12,6 +12,8 @@ function SearchMain(): JSX.Element {
 
   //TO DO: перейти на стор + всю логику поиска карточек перенести в селектор
   const [search, setSearch] = useState<string>('');
+  const [searchLineIndex, setSearchLineIndex] = useState(0);
+  // const searchLineIndexRef = useRef(-1)
   const cameras = useAppSelector(getCameras);
 
   const filterBySearchList = cameras.filter((item) => {
@@ -22,8 +24,57 @@ function SearchMain(): JSX.Element {
 
   const isActive = formatSearch(search).length >= 1;
 
+  function handleKeydown(event: React.KeyboardEvent) {
+    const isUpKey = event.key.startsWith('ArrowUp');
+    const isDownKey = event.key.startsWith('ArrowDown');
+
+    if (isDownKey) {
+      event.preventDefault();
+      setSearchLineIndex(prev => prev + 1);
+      // searchLineIndexRef.current = searchLineIndexRef.current + 1;
+      console.log(searchLineIndex);
+    }
+    if (isUpKey) {
+      event.preventDefault();
+      setSearchLineIndex(prev => prev - 1);
+      // searchLineIndexRef.current = searchLineIndexRef.current - 1;
+      console.log(searchLineIndex);
+    }
+  }
+
+  const searchListRef = useRef<HTMLDivElement>(null);
+  const closeSearchList = setSearch;
+
+
+  // закрытие поиска по клику outside
+  function handleOutsideClick(event: MouseEvent) {
+    event.preventDefault();
+    const element = searchListRef.current;
+    if (element && !element.contains(event.target as Element)) {
+      closeSearchList('');
+    }
+
+  }
+  useEffect(() => {
+    let isMounted = true;
+    const element = searchListRef.current;
+
+    if (isMounted && element) {
+      document.addEventListener('click', handleOutsideClick);
+    }
+
+    return () => {
+      isMounted = false;
+      document.removeEventListener('click', handleOutsideClick);
+    };
+  });
+
   return (
-    <div className={cn('form-search', { 'list-opened': isActive })}>
+    <div
+      className={cn('form-search', { 'list-opened': isActive })}
+      onKeyDown={handleKeydown}
+      ref={searchListRef}
+    >
       <form>
         <label>
           <svg className="form-search__icon" width="16" height="16" aria-hidden="true">
@@ -39,7 +90,7 @@ function SearchMain(): JSX.Element {
 
           />
         </label>
-        {isActive && <SearchList list={filterBySearchList} onSearchListClick={setSearch}/>}
+        {isActive && <SearchList list={filterBySearchList} onSearchListClick={setSearch} searchLineIndex={searchLineIndex}/>}
       </form>
       <button
         className="form-search__reset"
