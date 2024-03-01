71f3a9ed91bf5fac6166c4abd13e716e70c3ed57
Sahara <sahara.ap13@gmail.com>
2024-03-01 08:49:57 +0300

    Добавлены тесты в appDataSlice

--- src/store/app-data-store/app-data-slice.test.ts
+++ src/store/app-data-store/app-data-slice.test.ts
@@ -1,8 +1,9 @@
 import { SortingOrder, SortingType } from '../../consts';
+import { TCameraCategory, TCameraLevel, TCameraType } from '../../types/general-types';
 import { makeFakeCards, makeFakeReviews, makeFakeSelectedCard, makeFakeSimilars } from '../../utils/mocks';
 import { fetchCamerasAction, fetchSelectedCameraAction, fetchSimilars } from '../api-actions/card-actions';
 import { fetchReviews } from '../api-actions/reviews-action';
-import { appDataSlice, setErrorServerResponse } from './app-data-slice';
+import { appDataSlice, setCategoryFilterList, setErrorServerResponse, setLevelFilterList, setPriceMaxFilter, setPriceMinFilter, setSortOrder, setSortType, setTypeFilterList } from './app-data-slice';
 
 describe('app-data-slice', () => {
   const initialState = {
@@ -51,6 +52,56 @@ describe('app-data-slice', () => {
       }
     );
   });
+  it('should set "priceMinFilter" to payloadValue with "setPriceMinFilter" action',
+    () => {
+      const expectedResult = '1';
+      const result = appDataSlice.reducer(undefined, setPriceMinFilter(expectedResult));
+      expect(result.priceMinFilter).toBe(expectedResult);
+    }
+  );
+  it('should set "priceMaxFilter" to payloadValue with "setPriceMaxFilter" action',
+    () => {
+      const expectedResult = '100';
+      const result = appDataSlice.reducer(undefined, setPriceMaxFilter(expectedResult));
+      expect(result.priceMaxFilter).toBe(expectedResult);
+    }
+  );
+  it('should set "categoryFilterList" to payloadValue with "setCategoryFilterList" action',
+    () => {
+      const expectedResult = ['Видеокамера'] as TCameraCategory[];
+      const result = appDataSlice.reducer(undefined, setCategoryFilterList(expectedResult));
+      expect(result.categoryFilterList).toEqual(expectedResult);
+    }
+  );
+  it('should set "typeFilterList" to payloadValue with "setTypeFilterList" action',
+    () => {
+      const expectedResult = ['Коллекционная'] as TCameraType[];
+      const result = appDataSlice.reducer(undefined, setTypeFilterList(expectedResult));
+      expect(result.typeFilterList).toEqual(expectedResult);
+    }
+  );
+  it('should set "levelFilterList" to payloadValue with "setLevelFilterList" action',
+    () => {
+      const expectedResult = ['Нулевой'] as TCameraLevel[];
+      const result = appDataSlice.reducer(undefined, setLevelFilterList(expectedResult));
+      expect(result.levelFilterList).toEqual(expectedResult);
+    }
+  );
+
+  it('should set "sortType" to payloadValue with "setSortType" action',
+    () => {
+      const expectedResult = SortingType.Price;
+      const result = appDataSlice.reducer(undefined, setSortType(expectedResult));
+      expect(result.sortType).toBe(expectedResult);
+    }
+  );
+  it('should set "sortOrder" to payloadValue with "setSortOrder" action',
+    () => {
+      const expectedResult = SortingOrder.Non;
+      const result = appDataSlice.reducer(undefined, setSortOrder(expectedResult));
+      expect(result.sortOrder).toBe(expectedResult);
+    }
+  );
 
   describe('extraReducers', () => {
 
