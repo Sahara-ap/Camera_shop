9805671bbb772759e1f6661612e3e8310059ab1e
Sahara <sahara.ap13@gmail.com>
2024-03-05 21:14:25 +0300

    Реализована логика изменения состояния чекбоксов в группе FilterType

--- src/components/catalog-filter-aside/filter-type/filter-type.tsx
+++ src/components/catalog-filter-aside/filter-type/filter-type.tsx
@@ -2,7 +2,7 @@ import { useSearchParams } from 'react-router-dom';
 import { getParams } from '../../../utils/utils-functions';
 import { useAppDispatch, useAppSelector } from '../../../hooks/store-hooks';
 import { getTypeFilterList } from '../../../store/app-data-store/app-data-selectors';
-import { useEffect } from 'react';
+import { useEffect} from 'react';
 import { setTypeFilterList } from '../../../store/app-data-store/app-data-slice';
 import { TCameraType } from '../../../types/general-types';
 import { updateCheckedList, updateFilterParam } from '../utils/filter-utils';
@@ -47,6 +47,7 @@ function FilterType(): JSX.Element {
   const dispatch = useAppDispatch();
   const typeFilterList = useAppSelector(getTypeFilterList);
 
+
   const [searchParams, setSearchParams] = useSearchParams();
   const params = getParams(searchParams);
 
@@ -54,6 +55,19 @@ function FilterType(): JSX.Element {
     const typeParams = params.type?.split('-') as TCameraType[] || [];
     dispatch(setTypeFilterList(typeParams));
   }, [dispatch, params.type]);
+
+  useEffect(() => {
+    const hasVideocamera = params.cat?.includes('Видеокамера');
+
+    if (hasVideocamera) {
+      const typeParams = params.type?.split('-') as TCameraType[] || [];
+      const withoutFilmAndSnapTypeParams = typeParams.filter((type) => (type !== TypeParam.Film && type !== TypeParam.Snap));
+
+      updateFilterParam(params, 'type', withoutFilmAndSnapTypeParams);
+      setSearchParams(params);
+    }
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [params.cat, params.type, setSearchParams]);
 
   function handleFilterToggle(title: TCameraType) {
     const typeCheckedList = [...typeFilterList];
