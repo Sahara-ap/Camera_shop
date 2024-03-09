61b682df6a04f25724ba4e573f612100da07585d
Sahara <sahara.ap13@gmail.com>
2024-03-09 16:06:56 +0300

    Реализован переход на страницу в момент нажатия кнопки Корзина в самой карточке

--- src/components/card/card.tsx
+++ src/components/card/card.tsx
@@ -68,7 +68,10 @@ function Card({ cardData, page }: TCardProps): JSX.Element {
             </button>
           )
           :
-          <Link className="btn btn--purple-border product-card__btn product-card__btn--in-cart" to="#">
+          <Link
+            className="btn btn--purple-border product-card__btn product-card__btn--in-cart"
+            to={AppRoute.Basket}
+          >
             <svg width="16" height="16" aria-hidden="true">
               <use xlinkHref="#icon-basket"></use>
             </svg>В корзине
