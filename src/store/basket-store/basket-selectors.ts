e8e1b462fa5d942dab8807fc7edfd7dc9266a62f
Sahara <sahara.ap13@gmail.com>
2024-03-10 19:07:40 +0300

    Реализована в селекторе логика проверки на нулевой count

--- src/store/basket-store/basket-selectors.ts
+++ src/store/basket-store/basket-selectors.ts
@@ -1,7 +1,7 @@
 import { createSelector } from '@reduxjs/toolkit';
 import { NameSpace } from '../../consts';
 import { State } from '../../types/store';
-import { TSelectedCard } from '../../types/general-types';
+import { TBasketCard, TCard, TSelectedCard } from '../../types/general-types';
 
 const getBasketList = (state: Pick<State, NameSpace.Basket>) => state[NameSpace.Basket].basketList;
 const getBasketListId = createSelector([getBasketList], (basketList) => {
@@ -9,6 +9,25 @@ const getBasketListId = createSelector([getBasketList], (basketList) => {
   return idList;
 });
 const getBasketCount = createSelector([getBasketList], (basketList) => basketList.length);
+
+// const getBasketListUpgrade = createSelector([((state: Pick<State, NameSpace.Basket>) => state[NameSpace.Basket].basketList)],
+//   (basketList) => {
+//     const basketListWithoutNullOfCount = basketList.filter((camera) => camera.count !== 0);
+//     return basketListWithoutNullOfCount;
+//   });
+const getBasketListUpgrade = createSelector([(state: Pick<State, NameSpace.Basket>) => state[NameSpace.Basket]], (state) => {
+  const basketList = state.basketList;
+  const basketListWithoutNullOfCount = basketList.filter((camera: TBasketCard) => camera.count !== 0);
+  return basketListWithoutNullOfCount;
+});
+
+const getTotalCount = createSelector([getBasketListUpgrade], (basketList) => {
+  const totalCount = basketList.reduce((sum, camera) => {
+    const result = sum + camera.count;
+    return result;
+  }, 0);
+  return totalCount;
+});
 
 const getBasketListWithUniqueItems = createSelector([getBasketList, getBasketListId], (basketList, ids) => {
   // const uniqueIds = Array.from(new Set(ids));
@@ -50,4 +69,7 @@ export {
   getBasketCount,
 
   getBasketListWithUniqueItems,
+  getBasketListUpgrade,
+  getTotalCount,
+
 };
