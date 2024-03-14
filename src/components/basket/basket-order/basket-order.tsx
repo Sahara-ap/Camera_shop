133a0e5de98f41140f96509ab0c437fd06da78dc
Sahara <sahara.ap13@gmail.com>
2024-03-14 21:45:37 +0300

    Кнопка Оформить заказ заблокирована если корзина пуста

--- src/components/basket/basket-order/basket-order.tsx
+++ src/components/basket/basket-order/basket-order.tsx
@@ -2,12 +2,13 @@ import './basket-order.css';
 
 import cn from 'classnames';
 import { useAppSelector } from '../../../hooks/store-hooks';
-import { getDiscount, getTotalSum } from '../../../store/basket-store/basket-selectors';
+import { getBasketList, getDiscount, getTotalSum } from '../../../store/basket-store/basket-selectors';
 import { formatPrice } from '../../../utils/utils-functions';
 
 function BasketOrder(): JSX.Element {
   const totalSum = useAppSelector(getTotalSum);
   const discount = useAppSelector(getDiscount);
+  const basketList = useAppSelector(getBasketList);
   return (
     <div className="basket__summary-order">
       <p className="basket__summary-item">
@@ -28,7 +29,12 @@ function BasketOrder(): JSX.Element {
         <span className="basket__summary-text basket__summary-text--total">К оплате:</span>
         <span className="basket__summary-value basket__summary-value--total">{formatPrice(totalSum * (1 - discount))} ₽</span>
       </p>
-      <button className="btn btn--purple" type="submit">Оформить заказ
+      <button
+        className="btn btn--purple"
+        type="submit"
+        disabled={basketList.length === 0}
+      >
+        Оформить заказ
       </button>
     </div>
   );
