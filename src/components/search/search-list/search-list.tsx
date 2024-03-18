8f3c00608d409f59c2aba04aa9f4661f22ad09c5
Sahara <sahara.ap13@gmail.com>
2024-03-18 09:08:35 +0300

    Реализована отрисовка фразы 'no matches' в кейсе: нет карточек в строке поиска

--- src/components/search/search-list/search-list.tsx
+++ src/components/search/search-list/search-list.tsx
@@ -6,14 +6,17 @@ type TSearchListProps = {
   searchLineIndex: number;
 }
 function SearchList({ list, searchLineIndex }: TSearchListProps): JSX.Element {
+  const isEmptyList = list.length === 0;
+
 
   return (
     <ul
       className="form-search__select-list"
       data-testid="searchListUl"
+      style={isEmptyList ? {overflow: 'hidden', color: 'grey'} : {}}
 
     >
-      {list.map((item, index) => (
+      {isEmptyList ? <li className="form-search__select-item">no matches</li> : list.map((item, index) => (
         <SearchItem key={item.id} item={item} isSelected={index === searchLineIndex} />
       ))}
     </ul>
